aca9af6dfe2c7b5f8678f7f3817b5581e136ec5c
minhee231 <goominhee123@gmail.com>
2024-04-04 13:50:01 +0900

    머리 수확 페이지 추가

---
+++ frontned/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route('/')
+def index_page():
+    return render_template('index.html')
+
+@app.route('/login')
+def login_page():
+    return render_template('login.html')
+
+@app.route('/harvest-hair')
+def harvest_hair_page():
+    encryption_key = "알아서 지정"
+    return render_template('harvest_hair.html')
+
+
+
+if __name__ == '__main__':
+    app.run(port=3400)
