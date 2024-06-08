267bf1743cc855f5a869ce5162fd90ec286572d1
minhee231 <goominhee123@gmail.com>
2024-06-08 17:22:51 +0900

    test: module 지워보기

--- backend/setup.py
+++ backend/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="JihwanTModule",
     version="0.1",
-    packages=find_packages("module"),
+    packages=find_packages(),
     install_requires=[
         # 여기에 외부 패키지들을 추가할 수 있습니다.
     ],
