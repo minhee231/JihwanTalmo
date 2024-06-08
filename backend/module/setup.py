c3a67ee2a7b2c2860a5f47191b2a0685f22ba41e
minhee231 <goominhee123@gmail.com>
2024-06-08 16:30:55 +0900

    :rotating_light:test: 패키지 경로 수정

--- backend/module/setup.py
+++ backend/module/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(
     name="JihwanTModule",
     version="0.1",
-    packages=["backend.module"],
+    packages=[""],
     install_requires=[
         "CtrlJson.py",
         "Encryption.py"
