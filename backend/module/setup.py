339792ad707c80e086676c6846f33fa58a0566ca
minhee231 <goominhee123@gmail.com>
2024-06-08 16:16:42 +0900

    확장자 삭제

--- backend/module/setup.py
+++ backend/module/setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.1",
     packages=find_packages(),
     install_requires=[
-        "CtrlJson.py",
-        "Encryption.py"
+        "CtrlJson",
+        "Encryption"
     ],
 )
