b08f7fed92283c0d11f4024f5f856762e26c4ca4
minhee231 <goominhee123@gmail.com>
2024-06-08 17:13:48 +0900

    test: url 추가

--- backend/module/setup.py
+++ backend/module/setup.py
@@ -3,8 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name="JihwanTModule",
     version="0.1",
-    packages=find_packages("backend/module"),
+    packages=["JihwanTModule"],
+    url="https://github.com/minhee231/JihwanTalmo/tree/main/backend/module",
     install_requires=[
-        # 여기에 외부 패키지들을 추가할 수 있습니다.
     ],
 )
