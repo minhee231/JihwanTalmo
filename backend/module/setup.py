68dc7c9943d17f2ac95b5a06bb6fcc277656d52d
minhee231 <goominhee123@gmail.com>
2024-06-10 11:05:59 +0900

    setup.py 제거

--- backend/module/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="JihwanTModule",
-    version="0.1",
-    packages=find_packages(),  # 현재 디렉토리의 모든 패키지를 찾아 포함합니다.
-    url="https://github.com/minhee231/JihwanTalmo/tree/main/backend/module",  # 모듈의 소스코드가 위치한 URL을 명시합니다.
-)
