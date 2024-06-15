75a99cd01df25e7cbc00ce753d590afd4b62a535
minhee231 <goominhee123@gmail.com>
2024-06-15 15:58:27 +0900

    :sparkles:feat: IndexPage 데일리 작업에 인증기능 추가

--- backend/Lambda/lambda_function.py
+++ backend/Lambda/lambda_function.py
@@ -2,6 +2,10 @@ import json
 import requests
 
 def lambda_handler(event, context):
-    url = "https://indexp.jihwan-talmo.x-coll.com/add/talmo-gione"
+    with open("./key.json", "r", encoding='utf-8') as file:
+        key_data = json.load(file)
+
+    INDEX_PAGE_KEY = key_data["index_page"]
+    url = f"https://indexp.jihwan-talmo.x-coll.com/add/talmo-gione?auth_key={INDEX_PAGE_KEY}"
     r = requests.get(url)
     return r.text
